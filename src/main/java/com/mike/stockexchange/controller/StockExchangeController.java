b2f9f77dcce952a9aa054e4e7786dcc66d277f7d
houbin.dong <dhoubin@cn.ibm.com>
2019-12-08 19:19:25 +0800

    add log

--- src/main/java/com/mike/stockexchange/controller/StockExchangeController.java
+++ src/main/java/com/mike/stockexchange/controller/StockExchangeController.java
@@ -2,6 +2,8 @@ package com.mike.stockexchange.controller;
 
 import com.mike.stockexchange.entity.StockExchange;
 import com.mike.stockexchange.service.StockExchangeService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -22,24 +24,29 @@ import java.util.List;
 @Controller
 public class StockExchangeController {
 
+    private static final Logger log = LoggerFactory.getLogger(StockExchangeController.class);
+
     @Autowired
     private StockExchangeService stockExchangeService;
 
     @PostMapping("/stockexchange")
     public ResponseEntity<Void> addStockExchange(@RequestBody StockExchange stockExchange) {
         stockExchangeService.createStockExchange(stockExchange);
+        log.info("Create StockExchange successfully!");
         return new ResponseEntity<Void>(HttpStatus.CREATED);
     }
 
     @PutMapping("/stockexchange")
     public ResponseEntity<Void> editStockExchange(@RequestBody StockExchange stockExchange) {
         stockExchangeService.editStockExchange(stockExchange);
+        log.info("Edit StockExchange successfully!");
         return new ResponseEntity<Void>(HttpStatus.CREATED);
     }
 
     @GetMapping("/stockexchanges")
     public ResponseEntity<List<StockExchange>> getStockExchangesList() {
         List<StockExchange> stockExchangeList = new ArrayList<>();
+        log.info("Get stockexchanges list successfully!");
         stockExchangeList = stockExchangeService.retrieveAllStockExchanges();
         return new ResponseEntity<>(stockExchangeList, HttpStatus.OK);
     }
